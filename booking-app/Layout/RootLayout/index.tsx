c515757c5a92bdf94fec764d82398006c8f117b8
Aydan Abbaszadə <aydanaab@code.edu.az>
2026-02-28 00:48:16 +0400

    feat: create stays page layout

---
+++ booking-app/Layout/RootLayout/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header/page";
+import Footer from "./Footer/page";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
+}
---
+++ booking-app/app/(main)/stays/page.tsx
@@ -0,0 +1,3 @@
+export default function Stays() {
+  return <div>Stays</div>;
+}
