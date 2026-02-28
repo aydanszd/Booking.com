1fa3f49f77219d994bf80e430d596914dd2493a7
Aydan Abbaszadə <aydanaab@code.edu.az>
2026-02-28 16:50:06 +0400

    feat: complete UI until Offers section

--- booking-app/app/(main)/stays/page.tsx
+++ booking-app/app/(main)/stays/page.tsx
@@ -1,5 +1,12 @@
 import RecentSearches from "@/sections/stays/Yourrecentsearches";
-
+import StillInterestedSection from "@/sections/stays/AreYouStillInterested";
+import OptionsSection from "@/sections/stays/Opportunities";
 export default function Page() {
-  return <RecentSearches />;
+  return (
+    <>
+      <RecentSearches />
+      <StillInterestedSection />
+      <OptionsSection />
+    </>
+  );
 }
